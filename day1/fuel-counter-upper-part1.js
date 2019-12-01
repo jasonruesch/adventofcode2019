3bdcb79a953a18e248d0a0a3e2b0ec9421871e1f
Jason Ruesch <jason.ruesch@me.com>
2019-12-01 18:14:01 -0500

    Update fuel-counter-upper-part1.js

--- day1/fuel-counter-upper-part1.js
+++ day1/fuel-counter-upper-part1.js
@@ -1,8 +1,10 @@
-let fs = require('fs');
+const fs = require('fs');
 
 fs.readFile('input', function (_, data) {
   let totalFuel = 0;
   data.toString().split('\n').forEach(function (mass) {
+    if (mass === '') { return; }
+    
     const fuel = Math.floor(mass / 3) - 2;
     totalFuel += fuel;
   });
